f97f123ede69994d8d70934d2873dd596af7e6ee
AnayTaparia <anay.taparia@gmail.com>
2026-05-27 14:17:19 +0530

    :)

--- Chess copy.py
+++ Chess copy.py
@@ -1,143 +1,196 @@
 '''Chess by anay.taparia@gmail.com
-8x8x2 3D board version with board ! and board @'''
+The 8x8 game'''
 import sys
 WIDTH = 8
 HEIGHT = 8
 BLANK = ' '
 WHITE = "white"
 BLACK = "black"
-BOARD_BOTTOM = 0  # board !
-BOARD_TOP = 1     # board @
-
-
-def square_to_coord(square):
-    if len(square) != 2:
-        raise ValueError("invalid square")
-    file = square[0].lower()
-    rank = square[1]
-    if file < 'a' or file > 'h' or rank < '1' or rank > '8':
-        raise ValueError("invalid square")
-    return ord(file) - ord('a'), int(rank) - 1
 
 
 class Piece:
-    def __init__(self, kind, color):
+    def __init__(self,kind,color):
         self.kind = kind
         self.color = color
 
     def __str__(self):
         return self.kind.upper() if self.color == WHITE else self.kind.lower()
-
+    
     def get_unicode_str(self):
-        str_to_unicode_W = {"K": "♚", "Q": "♛", "R": "♜", "B": "♝", "N": "♞", "P": "♟"}
-        str_to_unicode_B = {"K": "♔", "Q": "♕", "R": "♖", "B": "♗", "N": "♘", "P": "♙"}
-        return str_to_unicode_W[self.kind] if self.color == WHITE else str_to_unicode_B[self.kind]
-
-
+        str_to_unicode_W = {"K":"♚","Q":"♛","R":"♜","B":"♝","N":"♞","P":"♟"}
+        str_to_unicode_B = {"K":"♔","Q":"♕","R":"♖","B":"♗","N":"♘","P":"♙"}
+        if self.color == WHITE:
+            return str_to_unicode_W[self.kind]
+        if self.color == BLACK:
+            return str_to_unicode_B[self.kind]
+    
 class Board:
-    def __init__(self, turn=WHITE):
-        self.layers = [self.new_board(), self.empty_board()]
+    def __init__(self,turn=WHITE):
+        self.board = self.new_board()
         self.turn = turn
+        #useful for special moves later on
+        self.wks = True
+        self.wqs = True
+        self.bks = True
+        self.bqs = True
+        self.enpass = False
 
     def new_board(self):
-        return [
-            [Piece('R', WHITE), Piece('N', WHITE), Piece('B', WHITE), Piece('Q', WHITE), Piece('K', WHITE), Piece('B', WHITE), Piece('N', WHITE), Piece('R', WHITE)],
-            [Piece('P', WHITE), Piece('P', WHITE), Piece('P', WHITE), Piece('P', WHITE), Piece('P', WHITE), Piece('P', WHITE), Piece('P', WHITE), Piece('P', WHITE)],
-            [None] * WIDTH,
-            [None] * WIDTH,
-            [None] * WIDTH,
-            [None] * WIDTH,
-            [Piece('P', BLACK), Piece('P', BLACK), Piece('P', BLACK), Piece('P', BLACK), Piece('P', BLACK), Piece('P', BLACK), Piece('P', BLACK), Piece('P', BLACK)],
-            [Piece('R', BLACK), Piece('N', BLACK), Piece('B', BLACK), Piece('Q', BLACK), Piece('K', BLACK), Piece('B', BLACK), Piece('N', BLACK), Piece('R', BLACK)],
-        ]
-
-    def empty_board(self):
-        return [[None] * WIDTH for _ in range(HEIGHT)]
+        return [[Piece('R',WHITE),Piece('N',WHITE),Piece('B',WHITE),Piece('Q',WHITE),Piece('K',WHITE),Piece('B',WHITE),Piece('N',WHITE),Piece('R',WHITE)],
+                [Piece('P',WHITE),Piece('P',WHITE),Piece('P',WHITE),Piece('P',WHITE),Piece('P',WHITE),Piece('P',WHITE),Piece('P',WHITE),Piece('P',WHITE)],
+                [None,None,None,None,None,None,None,None],
+                [None,None,None,None,None,None,None,None],
+                [None,None,None,None,None,None,None,None],
+                [None,None,None,None,None,None,None,None],
+                [Piece('P',BLACK),Piece('P',BLACK),Piece('P',BLACK),Piece('P',BLACK),Piece('P',BLACK),Piece('P',BLACK),Piece('P',BLACK),Piece('P',BLACK)],
+                [Piece('R',BLACK),Piece('N',BLACK),Piece('B',BLACK),Piece('Q',BLACK),Piece('K',BLACK),Piece('B',BLACK),Piece('N',BLACK),Piece('R',BLACK)],]
 
     def print_board(self):
-        rowsep = '  +---+---+---+---+---+---+---+---+'
-        header = '    a   b   c   d   e   f   g   h'
-        print('Board @ (top layer)'.ljust(40) + 'Board ! (bottom layer)')
-        print(header + '     ' + header)
-        print(rowsep + '     ' + rowsep)
-        for rank in range(HEIGHT - 1, -1, -1):
-            top_line = f"{rank + 1} |"
-            bottom_line = f"{rank + 1} |"
-            for file in range(WIDTH):
-                top_piece = self.layers[BOARD_TOP][rank][file]
-                bottom_piece = self.layers[BOARD_BOTTOM][rank][file]
-                top_line += f" {top_piece.get_unicode_str()} " if top_piece else '   '
-                top_line += '|'
-                bottom_line += f" {bottom_piece.get_unicode_str()} " if bottom_piece else '   '
-                bottom_line += '|'
-            print(top_line + '     ' + bottom_line)
-            print(rowsep + '     ' + rowsep)
-
-
+        rowsep='  +---+---+---+---+---+---+---+---+'
+        print('    a   b   c   d   e   f   g   h')
+        print(rowsep)
+        for y,rank in enumerate(self.board):
+            print(HEIGHT-(y),"|",end="")
+            for x,square in enumerate(rank):
+                if self.board[7-y][x] is None:
+                    print(BLANK*3,end="|")
+                else:
+                    print(f" {self.board[7-y][x].get_unicode_str()} ",end="|")
+            print()
+            print(rowsep)
+    
 class Move:
-    def __init__(self, move_str):
-        move_str = move_str.strip()
-        if move_str.endswith('@@'):
-            self.s_layer = BOARD_BOTTOM
-            self.d_layer = BOARD_TOP
-            self.sf, self.sr = square_to_coord(move_str[:2])
-            self.ef, self.er = self.sf, self.sr
-        elif move_str.endswith('!!'):
-            self.s_layer = BOARD_TOP
-            self.d_layer = BOARD_BOTTOM
-            self.sf, self.sr = square_to_coord(move_str[:2])
-            self.ef, self.er = self.sf, self.sr
-        elif len(move_str) == 4:
-            self.s_layer = BOARD_BOTTOM
-            self.d_layer = BOARD_BOTTOM
-            self.sf, self.sr = square_to_coord(move_str[:2])
-            self.ef, self.er = square_to_coord(move_str[2:])
-        else:
-            raise ValueError('invalid move format')
+    def __init__(self,stri):
+        self.sf=ord(stri[0])-ord("a")
+        self.sr=int(stri[1])-1
+        self.ef=ord(stri[2])-ord("a")
+        self.er=int(stri[3])-1
 
     def unpck(self):
-        return self.s_layer, self.sf, self.sr, self.d_layer, self.ef, self.er
-
-
+        return (self.sf,self.sr,self.ef,self.er)
+    
 class Game:
     def __init__(self):
-        self.board = Board()
+        self.board = Board() 
 
     def get_input(self):
         while True:
-            move = input(
-                "enter your move or 'quit'\n"
-                "- normal board move (board !): e2e4\n"
-                "- move from board ! to board @ : e5@@\n"
-                "- move from board @ to board ! : e5!!\n> "
-            )
+            move = input("enter your move or 'quit'\n> ")
             if move == 'quit':
                 sys.exit()
             try:
                 move = Move(move)
-                return move
-            except Exception:
+                break
+            except:
                 print("invalid move")
+                continue
+        return move
+    
+    def is_move_legal(self, move):
+        board = self.board.board
+        sf,sr,ef,er = move.unpck()
+        target = board[er][ef]        
+        capture = not (target==None)
+        piece = board[sr][sf]
+        dr = er-sr
+        df = sf-ef
+        if target != None:
+            if target.color == piece.color:
+                return False # pieces cannot capture pieces of their own color
+            
+        elif piece.kind == "N": # added knight logic only
+            if (abs(dr),abs(df)) in [(2,1),(1,2)]:
+                return True
+            else:
+                return False
+        elif piece.kind == "P":
+            if piece.color == WHITE:
+                if capture: # capture
+                    if (dr,df) in [(1,1),(1,-1)]:
+                        return True
+                    else:
+                        return False
+                else: # not capture
+                    if (dr,df) == (1,0):
+                        return True
+                    else:
+                        return False
+            if piece.color == BLACK:
+                if target: # capture
+                    if (dr,df) in [(-1,1),(-1,-1)]:
+                        return True
+                    else:
+                        return False
+                else: # not capture
+                    if (dr,df) == (-1,0):
+                        return True
+                    else:
+                        return False
+        return True # everything else is legal
+    
+    def give_sliding_moves(self, dr, df, sr, sf):
+        '''Gives a list of sliding moves from sx,sy in direction dx,dy until blocked'''
+        bord = self.board.board
+        moves = []
+        moves.append((sr,sf))
+        colr = bord[sf][sr].color
+        target = bord[sf][sr]
+        while target == None or target.color != colr:
+            sr,sf = sr+dr,sf+df
+            moves.append((sr,sf))
+            target = bord[sf][sr]
+        return moves
 
-    def make_move(self, move):
-        s_layer, sf, sr, d_layer, ef, er = move.unpck()
-        source_piece = self.board.layers[s_layer][sr][sf]
-        if source_piece is None:
-            print('no piece at source square')
-            return
+    def make_move(self,move):
+        sf,sr,ef,er = move.unpck()
+        capture = bool(self.board.board[er][ef])
+        
+        if self.board.board[sr][sf].kind == 'P': # Promotion
+            if self.board.board[sr][sf].color == WHITE and er == 7:
+                self.board.board[er][ef]=Piece(input("enter promotion piece in upcase\n> "),WHITE)
+                self.board.board[sr][sf]=None
+                return None
+            elif self.board.board[sr][sf].color == BLACK and er == 0:
+                self.board.board[er][ef]=Piece(input("enter promotion piece in upcase\n> "),BLACK)
+                self.board.board[sr][sf]=None
+                return None
+                
 
-        self.board.layers[d_layer][er][ef] = source_piece
-        self.board.layers[s_layer][sr][sf] = None
+        if self.board.board[sr][sf].kind == 'K': # remove castling rights
+            if self.board.board[sr][sf].color == WHITE:
+                self.board.wks = False
+                self.board.wqs = False
+            elif self.board.board[sr][sf].color == BLACK:
+                self.board.bks = False
+                self.board.bqs = False
+        # i wont add rook castle logic yet
+        if self.board.board[sr][sf].kind == 'P': # Using logic for en passant
+            # so it is if the pawn moves to an EMPTY square. delete the square behind it.
+            # if the move is normal. pawns move would get blocked if there was  a piece.
+            # and with a capture. it is not an empty square
+            if self.board.board[sr][sf].color == WHITE and not capture:
+                self.board.board[er][ef]=self.board.board[sr][sf]
+                self.board.board[er-1][ef]=None
+                self.board.board[sr][sf]=None
+                return None
+            elif self.board.board[sr][sf].color == BLACK and not capture:
+                self.board.board[er][ef]=self.board.board[sr][sf]
+                self.board.board[er+1][ef]=None
+                self.board.board[sr][sf]=None
+                return None
 
+        self.board.board[er][ef]=self.board.board[sr][sf]
+        self.board.board[sr][sf]=None
+        return None
+    
     def main(self):
         while True:
             self.board.print_board()
             move = self.get_input()
-            self.make_move(move)
-
+            if self.is_move_legal(move) or True: # i'm doing or true for sometime efore the legalmove detection is fully furnished. i'ts equivalent to just commenting it out
+                self.make_move(move)
 
 if __name__ == "__main__":
     chess = Game()
-    chess.main()
-
+    chess.main()       
 
